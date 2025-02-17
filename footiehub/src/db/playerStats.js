a818516450e2b7ad1475ed1e2ffdc08b4e168bd5
chazz579 <59661182+chazz579@users.noreply.github.com>
2025-02-16 22:55:41 -0500

    [FTH-10] display basic player stats rankings (goals, assists) on home page

--- footiehub/src/db/playerStats.js
+++ footiehub/src/db/playerStats.js
@@ -18,3 +18,41 @@ export async function getStats() {
         throw error;
     }
 }
+
+export async function getTopGoals() {
+    const query = 
+    `SELECT
+	    players.name AS player_name,
+	    SUM(player_performance.goals) AS goals
+     FROM players
+     LEFT JOIN player_performance 
+	    ON players.id = player_performance.player_id
+     GROUP BY players.id
+     ORDER BY goals DESC
+     LIMIT 5;`;
+    try {
+        return await db.query(query);
+    } catch (error) {
+        console.error('Error getting top goals stats: ', error);
+        throw error;
+    }
+}
+
+export async function getTopAssists() {
+    const query = 
+    `SELECT
+	    players.name AS player_name,
+	    SUM(player_performance.assists) AS assists
+     FROM players
+     LEFT JOIN player_performance 
+	    ON players.id = player_performance.player_id
+     GROUP BY players.id
+     ORDER BY assists DESC
+     LIMIT 5;`;
+    try {
+        return await db.query(query);
+    } catch (error) {
+        console.error('Error getting top assists stats: ', error);
+        throw error;
+    }
+}
