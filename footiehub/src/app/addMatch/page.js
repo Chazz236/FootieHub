22de72746d011946c9dfc5728bc35c5528bd90ec
chazz579 <197831996+Chazz236@users.noreply.github.com>
2025-05-02 15:08:26 -0400

    [FTH-35] update goal contributions and add validation

--- footiehub/src/app/addMatch/page.js
+++ footiehub/src/app/addMatch/page.js
@@ -15,8 +15,9 @@ const AddMatch = () => {
   const [awayScore, setAwayScore] = useState(0);
   const [homeTeam, setHomeTeam] = useState([]);
   const [awayTeam, setAwayTeam] = useState([]);
-  const [goalContributions, setGoalContributions] = useState([{ goal_scorer_id: 0, assist_player_id: 0 }]);
-
+  const [goalContributions, setGoalContributions] = useState([]);
+  const [homeGoals, setHomeGoals] = useState([]);
+  const [awayGoals, setAwayGoals] = useState([]);
   const [players, setPlayers] = useState([]);
 
   useEffect(() => {
@@ -28,15 +29,55 @@ const AddMatch = () => {
   }, []);
 
   useEffect(() => {
-    const totalGoals = homeScore + awayScore;
-    if (totalGoals !== goalContributions.length) {
-      const totalGoalContributions = Array(totalGoals).fill({ goal_scorer_id: 0, assist_player_id: 0 });
-      setGoalContributions(totalGoalContributions);
+    if (homeScore < homeGoals.length) {
+      setHomeGoals((goals) => {
+        const updatedGoals = [...goals];
+        while (homeScore < updatedGoals.length) {
+          updatedGoals.shift();
+        }
+        return updatedGoals;
+      });
     }
-  }, [homeScore, awayScore]);
+    else if (homeScore > homeGoals.length) {
+      setHomeGoals((goals) => {
+        const updatedGoals = [...goals];
+        while (homeScore > updatedGoals.length) {
+          updatedGoals.unshift({ goal_scorer_id: 0, assist_player_id: 0 });
+        }
+        return updatedGoals;
+      });
+    }
+  }, [homeScore]);
+
+  useEffect(() => {
+    if (awayScore < awayGoals.length) {
+      setAwayGoals((goals) => {
+        const updatedGoals = [...goals];
+        while (awayScore < updatedGoals.length) {
+          updatedGoals.shift();
+        }
+        return updatedGoals;
+      });
+    }
+    else if (awayScore > awayGoals.length) {
+      setAwayGoals((goals) => {
+        const updatedGoals = [...goals];
+        while (awayScore > updatedGoals.length) {
+          updatedGoals.unshift({ goal_scorer_id: 0, assist_player_id: 0 });
+        }
+        return updatedGoals;
+      });
+    }
+  }, [awayScore]);
+
+  useEffect(() => {
+    setGoalContributions([...homeGoals, ...awayGoals]);
+  }, [homeGoals, awayGoals])
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    console.log(goalContributions);
 
     if (!Array.isArray(goalContributions)) {
       console.error('goalContributions is not an array:', goalContributions);
@@ -68,8 +109,9 @@ const AddMatch = () => {
     //   return;
     // }
 
-    if (goalContributions.length !== homeScore + awayScore) {
-      alert('Goal contributions count does not match the total score:', goalContributions, homeScore + awayScore);
+    //should be unnecessary since goalcontributions show up when scores are changed
+    if (goalContributions.length !== parseInt(homeScore) + parseInt(awayScore)) {
+      alert('Goal contributions count does not match the total score:' + goalContributions.length + ' !== ' + parseInt(homeScore) + parseInt(awayScore));
       return;
     }
 
@@ -92,7 +134,8 @@ const AddMatch = () => {
     setAwayScore(0);
     setHomeTeam([]);
     setAwayTeam([]);
-    setGoalContributions([{ goal_scorer_id: 0, assist_player_id: 0 }]);
+    setHomeGoals([]);
+    setAwayGoals([]);
   };
 
   const handleHomeTeamChange = (playerID) => {
@@ -111,80 +154,111 @@ const AddMatch = () => {
     });
   }
 
-  const handleGoalContributionChange = (i, field, value) => {
-    const updatedGoalContributions = [...goalContributions];
-    updatedGoalContributions[i][field] = parseInt(value, 10);
-    setGoalContributions(updatedGoalContributions);
+  const handleHomeGoalContributionChange = (i, field, value) => {
+    const updatedHomeGoalContributions = [...homeGoals];
+    updatedHomeGoalContributions[i][field] = parseInt(value, 10);
+    setHomeGoals(updatedHomeGoalContributions);
+  }
+
+  const handleAwayGoalContributionChange = (i, field, value) => {
+    const updatedAwayGoalContributions = [...awayGoals];
+    updatedAwayGoalContributions[i][field] = parseInt(value, 10);
+    setAwayGoals(updatedAwayGoalContributions);
   }
 
   return (
     <main className='flex-1 p-6'>
       <form onSubmit={handleSubmit}>
-      <div className='flex gap-32'>
-        <div>
-        <div className='mb-6'>
-          <label>Date</label>
-          <input type='date' id='date' value={date} onChange={(e) => setDate(e.target.value)} className='ml-2 border border-black' required />
-        </div>
-
-        <div className='mb-6'>
-          <label>Home Score</label>
-          <input type='number' id='homeScore' value={homeScore} onChange={(e) => setHomeScore(parseInt(e.target.value, 10))} className='ml-2 border border-black' min='0' required />
-        </div>
-
-        <div className='mb-6'>
-          <label>Away Score</label>
-          <input type='number' id='awayScore' value={awayScore} onChange={(e) => setAwayScore(parseInt(e.target.value, 10))} className='ml-2 border border-black' min='0' required />
-        </div>
-
-        <div className='flex'>
+        <div className='flex gap-32'>
           <div>
-            <label>Home Team</label>
-            {players.map((player) => (
-              <div key={player.id}>
-                <input type='checkbox' id={player.id} checked={homeTeam.includes(player.id)} onChange={() => handleHomeTeamChange(player.id)} className='ml-2 border border-black' />
-                <label>{player.name}</label>
-              </div>
-            ))}
-          </div>
+            <div className='mb-6'>
+              <label>Date</label>
+              <input type='date' id='date' value={date} onChange={(e) => setDate(e.target.value)} className='ml-2 border border-black' required />
+            </div>
 
-          <div className='ml-6'>
-            <label>Away Team</label>
-            {players.map((player) => (
-              <div key={player.id}>
-                <input type='checkbox' id={player.id} checked={awayTeam.includes(player.id)} onChange={() => handleAwayTeamChange(player.id)} className='ml-2 border border-black' />
-                <label>{player.name}</label>
-              </div>
-            ))}
-          </div>
-        </div>
-        </div>
-        <div className='mb-6'>
-          <label>Goal Contributions</label>
-          {Array.isArray(goalContributions) && goalContributions.map((contribution, i) => (
-            <div key={i} className='mb-3'>
+            <div className='mb-6'>
+              <label>Home Score</label>
+              <input type='number' id='homeScore' value={homeScore} onChange={(e) => setHomeScore(parseInt(e.target.value, 10))} className='ml-2 border border-black' min='0' required />
+            </div>
+
+            <div className='mb-6'>
+              <label>Away Score</label>
+              <input type='number' id='awayScore' value={awayScore} onChange={(e) => setAwayScore(parseInt(e.target.value, 10))} className='ml-2 border border-black' min='0' required />
+            </div>
+
+            <div className='flex'>
               <div>
-                <label>Goal</label>
-                <select value={contribution.goal_scorer_id} onChange={(e) => handleGoalContributionChange(i, 'goal_scorer_id', e.target.value)} className='ml-2 border border-black'>
-                  <option key='' value=''>Goal</option>
-                  {players.map((player) => (
-                    <option key={player.id} value={player.id}>{player.name}</option>
-                  ))}
-                </select>
+                <label>Home Team</label>
+                {players.map((player) => (
+                  <div key={player.id}>
+                    <input type='checkbox' id={player.id} checked={homeTeam.includes(player.id)} onChange={() => handleHomeTeamChange(player.id)} className='ml-2 border border-black' />
+                    <label>{player.name}</label>
+                  </div>
+                ))}
               </div>
-              <div>
-                <label>Assist</label>
-                <select value={contribution.assist_player_id} onChange={(e) => handleGoalContributionChange(i, 'assist_player_id', e.target.value)} className='ml-2 border border-black'>
-                  <option key='' value=''>Assist</option>
-                  {players.map((player) => (
-                    <option key={player.id} value={player.id}>{player.name}</option>
-                  ))}
-                </select>
+
+              <div className='ml-6'>
+                <label>Away Team</label>
+                {players.map((player) => (
+                  <div key={player.id}>
+                    <input type='checkbox' id={player.id} checked={awayTeam.includes(player.id)} onChange={() => handleAwayTeamChange(player.id)} className='ml-2 border border-black' />
+                    <label>{player.name}</label>
+                  </div>
+                ))}
               </div>
             </div>
-          ))}
-        </div>
-        <button type='submit' className='border border-black self-start'>Add Match</button>
+          </div>
+          <div className='mb-6'>
+            <label>Home Goals</label>
+            {Array.isArray(homeGoals) && homeGoals.map((contribution, i) => (
+              <div key={i} className='mb-3'>
+                <div>
+                  <label>Goal</label>
+                  <select value={contribution.goal_scorer_id} onChange={(e) => handleHomeGoalContributionChange(i, 'goal_scorer_id', e.target.value)} className='ml-2 border border-black'>
+                    <option key='' value=''>Goal</option>
+                    {players.filter(player => homeTeam.includes(player.id) && player.id !== contribution.assist_player_id).map((player) => (
+                      <option key={player.id} value={player.id}>{player.name}</option>
+                    ))}
+                  </select>
+                </div>
+                <div>
+                  <label>Assist</label>
+                  <select value={contribution.assist_player_id} onChange={(e) => handleHomeGoalContributionChange(i, 'assist_player_id', e.target.value)} className='ml-2 border border-black'>
+                    <option key='' value=''>Assist</option>
+                    {players.filter(player => homeTeam.includes(player.id) && player.id !== contribution.goal_scorer_id).map((player) => (
+                      <option key={player.id} value={player.id}>{player.name}</option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className='mb-6'>
+            <label>Away Goals</label>
+            {Array.isArray(awayGoals) && awayGoals.map((contribution, i) => (
+              <div key={i} className='mb-3'>
+                <div>
+                  <label>Goal</label>
+                  <select value={contribution.goal_scorer_id} onChange={(e) => handleAwayGoalContributionChange(i, 'goal_scorer_id', e.target.value)} className='ml-2 border border-black'>
+                    <option key='' value=''>Goal</option>
+                    {players.filter(player => awayTeam.includes(player.id) && player.id !== contribution.assist_player_id).map((player) => (
+                      <option key={player.id} value={player.id}>{player.name}</option>
+                    ))}
+                  </select>
+                </div>
+                <div>
+                  <label>Assist</label>
+                  <select value={contribution.assist_player_id} onChange={(e) => handleAwayGoalContributionChange(i, 'assist_player_id', e.target.value)} className='ml-2 border border-black'>
+                    <option key='' value=''>Assist</option>
+                    {players.filter(player => awayTeam.includes(player.id) && player.id !== contribution.goal_scorer_id).map((player) => (
+                      <option key={player.id} value={player.id}>{player.name}</option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            ))}
+          </div>
+          <button type='submit' className='border border-black self-start'>Add Match</button>
         </div>
 
       </form>
