540b667f3e4413808a552f351a60a1fc1bb202b7
chazz579 <59661182+chazz579@users.noreply.github.com>
2025-05-19 15:04:35 -0400

    [FTH-40] sort matches and refactor code

--- footiehub/src/app/matches/page.js
+++ footiehub/src/app/matches/page.js
@@ -2,18 +2,47 @@
 
 import React, { useState, useEffect } from 'react';
 
+const DisplayGoals = ({ goals }) => (
+  <>
+    {goals.map((goal, i) => (
+      <div key={i}>
+        {goal.name}
+        {Array.from({ length: goal.goals }).map((_, j) => (
+          <img key={j} src='/ball.png' alt='Soccer Ball' className='inline-block w-3 h-3' />
+        ))}
+      </div>
+    ))}
+  </>
+);
+
+const GoalDetails = ({ goals }) => (
+  <table>
+    <tbody>
+      {goals.map((goal, j) => (
+        <tr>
+          <td>{goal.name}</td>
+          <td className='px-2'>{goal.goals}G</td>
+          <td className='px-2'>{goal.assists}A</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
 const Matches = () => {
   const [matches, setMatches] = useState([]);
   const [goals, setGoals] = useState([]);
   const [selectedMatch, setSelectedMatch] = useState(null);
-  const [matchResult, setMatchResult] = useState('');
 
-  useEffect(() => {
+  useEffect(() => { //change to server side?
     const getData = async () => {
       const res = await fetch('http://localhost:3000/api/matches');
       const data = await res.json();
       const { matches, goals } = data;
-      setMatches(matches);
+      const sortedMatches = [...matches].sort((a, b) => {
+        return new Date(a.date) - new Date(b.date);
+      })
+      setMatches(sortedMatches);
       setGoals(goals);
     };
     getData();
@@ -21,15 +50,6 @@ const Matches = () => {
 
   const handleClick = (match) => {
     setSelectedMatch(match);
-    if (match.home_score > match.away_score) {
-      setMatchResult('Home');
-    }
-    else if (match.home_score < match.away_score) {
-      setMatchResult('Away');
-    }
-    else {
-      setMatchResult('Draw');
-    }
   }
 
   return (
@@ -37,7 +57,7 @@ const Matches = () => {
       <table className='table-auto'>
         <tbody>
           {matches.map((match, i) => (
-            <React.Fragment key={i} /*instead of i, can use match.id*/>
+            <React.Fragment key={match.id}>
               <tr>
                 <td className='px-6'>{new Date(match.date).toLocaleDateString('en-GB', { day: '2-digit', month: 'long', year: 'numeric' })}</td>
                 <td className='px-6 text-right'>Home</td>
@@ -47,29 +67,21 @@ const Matches = () => {
               <tr key={i} className='text-center'>
                 <td></td>
                 <td className='px-6 text-xs align-text-top text-right'>
-                  {goals.filter((goal) => goal.team === 'home' && goal.match_id - 1 === i && goal.goals > 0)
-                    .map((goal, j) => (
-                      <div key={j}>
-                        {goal.name}
-                        {Array.from({ length: goal.goals }).map((_, i) => (
-                          <img src='/ball.png' alt='Soccer Ball' className='inline-block w-3 h-3' />
-                        ))}
-                      </div>
-                    ))}
+                  <DisplayGoals
+                    goals={goals.filter(
+                      (goal) => goal.team === 'home' && goal.match_id === match.id && goal.goals > 0
+                    )}
+                  />
                 </td>
                 <td className='px-6 text-xs align-text-top text-center text-blue-600'>
                   <button onClick={() => handleClick(match)}>Details</button>
                 </td>
                 <td className='px-6 text-xs align-text-top text-left'>
-                  {goals.filter((goal) => goal.team === 'away' && goal.match_id - 1 === i && goal.goals > 0)
-                    .map((goal, k) => (
-                      <div key={k}>
-                        {goal.name}
-                        {Array.from({ length: goal.goals }).map((_, i) => (
-                          <img src='/ball.png' alt='Soccer Ball' className='inline-block w-3 h-3' />
-                        ))}
-                      </div>
-                    ))}
+                  <DisplayGoals
+                    goals={goals.filter(
+                      (goal) => goal.team === 'away' && goal.match_id === match.id && goal.goals > 0
+                    )}
+                  />
                 </td>
               </tr>
             </React.Fragment>
@@ -77,7 +89,7 @@ const Matches = () => {
         </tbody>
       </table>
 
-      {selectedMatch !== null ?
+      {selectedMatch && (
         <div className='fixed top-10 right-0 w-1/3 h-full p-6'>
           <h2>Match Details</h2>
           <p>
@@ -85,50 +97,33 @@ const Matches = () => {
             {new Date(selectedMatch.date).toLocaleDateString('en-GB', { day: '2-digit', month: 'long', year: 'numeric' })}
           </p>
           <p>
-            <strong>Winner: </strong> {matchResult}
+            <strong>Winner: </strong>
+            {selectedMatch.home_score > selectedMatch.away_score
+              ? 'Home'
+              : selectedMatch.home_score < selectedMatch.away_score
+                ? 'Away'
+                : 'Draw'
+            }
           </p>
           <p>
             <strong>Final Score: <span className='font-normal'>{selectedMatch.home_score} - {selectedMatch.away_score}</span></strong>
           </p>
           <div className='flex space-x-24'>
-            <p>
+            <div>
               <strong>Home Team</strong>
-              <>
-                <table>
-                  <tbody>
-                    {goals.filter((goal) => goal.team === 'home' && goal.match_id === selectedMatch.id)
-                      .map((goal, j) => (
-                        <tr>
-                          <td>{goal.name}</td>
-                          <td className='px-2'>{goal.goals}G</td>
-                          <td className='px-2'>{goal.assists}A</td>
-                        </tr>
-                      ))}
-                  </tbody>
-                </table>
-              </>
-            </p>
-            <p>
+              <GoalDetails
+                goals={goals.filter((goal) => goal.team === 'home' && goal.match_id === selectedMatch.id)}
+              />
+            </div>
+            <div>
               <strong>Away Team</strong>
-              <>
-                <table>
-                  <tbody>
-                    {goals.filter((goal) => goal.team === 'away' && goal.match_id === selectedMatch.id)
-                      .map((goal, j) => (
-                        <tr>
-                          <td>{goal.name}</td>
-                          <td className='px-2'>{goal.goals}G</td>
-                          <td className='px-2'>{goal.assists}A</td>
-                        </tr>
-                      ))}
-                  </tbody>
-                </table>
-              </>
-            </p>
+              <GoalDetails
+                goals={goals.filter((goal) => goal.team === 'away' && goal.match_id === selectedMatch.id)}
+              />
+            </div>
           </div>
         </div>
-        :
-        <div></div>}
+      )}
 
     </main>
   )
