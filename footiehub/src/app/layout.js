22af3c0ee4f3ba893ea4a6933083baa6a93eb426
chazz579 <197831996+Chazz236@users.noreply.github.com>
2025-06-12 15:22:41 -0400

    [FTH-57] set up colours and fonts for app

--- footiehub/src/app/layout.js
+++ footiehub/src/app/layout.js
@@ -1,32 +1,32 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import Header from '../app/components/Header';
-import Sidebar from '../app/components/Sidebar';
-import "./globals.css";
+import './globals.css';
+import { Geist, Geist_Mono } from 'next/font/google';
+import Header from '@/app/components/Header';
+import Sidebar from '@/app/components/Sidebar';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata = {
-  title: "FootieHub",
-  description: "To prove who's the best",
+  title: 'FootieHub',
+  description: 'Soccer Statistics and Analysis Hub',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en' className={`${geistSans.variable} ${geistMono.variable}`}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`font-sans antialiased`}
       >
-        <div className="flex flex-col min-h-screen">
+        <div className='flex flex-col min-h-screen'>
           <Header />
-          <div className ="flex flex-1">
+          <div className='flex flex-1'>
             <Sidebar />
             {children}
           </div>
