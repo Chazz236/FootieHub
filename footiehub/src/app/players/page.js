77137c96858b0cc0a319aef868db79ff97f50748
Chazz236 <197831996+Chazz236@users.noreply.github.com>
2025-06-24 18:50:36 -0400

    [FTH-61] design players page visuals

--- footiehub/src/app/players/page.js
+++ footiehub/src/app/players/page.js
@@ -1,36 +1,36 @@
 import Link from 'next/link'
 import { getAllStats } from '@/lib/data/stats';
+import Table from '@/app/components/ui/Table';
 
 const Players = async () => {
-
   try {
     const stats = await getAllStats();
     return (
       <main className='flex-1 p-6'>
-        <table className='table-auto'>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Games</th>
-              <th>Goals</th>
-              <th>Assists</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
+        <Table>
+          <Table.Header>
+            <Table.HeaderRow>
+              <Table.HeaderCell>NAME</Table.HeaderCell>
+              <Table.HeaderCell className='text-center'>GAMES</Table.HeaderCell>
+              <Table.HeaderCell className='text-center'>GOALS</Table.HeaderCell>
+              <Table.HeaderCell className='text-center'>ASSISTS</Table.HeaderCell>
+              <Table.HeaderCell className='text-right border-r-0'>VALUE</Table.HeaderCell>
+            </Table.HeaderRow>
+          </Table.Header>
+          <Table.Body>
             {
               stats.map((player, i) => (
-                <tr key={i} className='text-center'>
-                  <td className='px-6'><Link href={`/players/${player.id}`}>{player.name}</Link></td>
-                  <td className='px-6'>{player.games}</td>
-                  <td className='px-6'>{player.goals ?? 0}</td>
-                  <td className='px-6'>{player.assists ?? 0}</td>
-                  <td className='px-6'>${Intl.NumberFormat().format(player.value)}</td>
-                </tr>
+                <Table.Row key={i}>
+                  <Table.Cell className='text-primary-accent font-semibold'><Link href={`/players/${player.id}`}>{player.name}</Link></Table.Cell>
+                  <Table.Cell className='text-center'>{player.games}</Table.Cell>
+                  <Table.Cell className='text-center'>{player.goals ?? 0}</Table.Cell>
+                  <Table.Cell className='text-center'>{player.assists ?? 0}</Table.Cell>
+                  <Table.Cell className={`${player.value > 0 ? 'text-success-color' : 'text-danger-color'} text-right font-semibold border-r-0`}>{player.value > 0 ? '$' : '-$'}{Intl.NumberFormat().format(Math.abs(player.value))}</Table.Cell>
+                </Table.Row>
               ))
             }
-          </tbody>
-        </table>
+          </Table.Body>
+        </Table>
       </main>
     )
   } catch (error) {
