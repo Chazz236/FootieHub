bab8f0a31843abf27b606d2347a2ff889f26f7ba
Chazz236 <197831996+Chazz236@users.noreply.github.com>
2025-08-15 16:49:39 -0400

    [FTH-84] optimize stat retrieval by consolidating backend queries

--- footiehub/src/db/playerStats.js
+++ footiehub/src/db/playerStats.js
@@ -3,97 +3,49 @@ import db from '@/db/mysql';
 export async function getStats() {
     const query =
         `SELECT
-        players.id AS id,
-	    players.name AS name,
-	    COUNT(player_performance.match_id) as games,
-	    SUM(player_performance.goals) AS goals,
-	    SUM(player_performance.assists) AS assists,
-        players.value AS value
-     FROM players
-     LEFT JOIN player_performance 
-	    ON players.id = player_performance.player_id
-     GROUP BY players.id;`;
+    players.id AS id,
+	players.name AS name,
+    players.value AS value,
+	COUNT(player_performance.match_id) AS games,
+    COUNT(CASE
+         WHEN player_performance.team = 'home' AND matches.home_score > matches.away_score THEN 1
+         WHEN player_performance.team = 'away' AND matches.away_score > matches.home_score THEN 1
+         ELSE NULL
+         END) AS wins,
+    COUNT(CASE
+         WHEN player_performance.team = 'home' AND matches.home_score = matches.away_score THEN 1
+         WHEN player_performance.team = 'away' AND matches.away_score = matches.home_score THEN 1
+         ELSE NULL
+         END) AS draws,
+    COUNT(CASE
+         WHEN player_performance.team = 'home' AND matches.home_score < matches.away_score THEN 1
+         WHEN player_performance.team = 'away' AND matches.away_score < matches.home_score THEN 1
+         ELSE NULL
+         END) AS losses,
+	COUNT(CASE
+         WHEN player_performance.team = 'home' AND matches.away_score = 0 THEN 1
+         WHEN player_performance.team = 'away' AND matches.home_score = 0 THEN 1
+         ELSE NULL
+         END) AS clean_sheets,
+    CAST(
+         CAST(COUNT(CASE
+            WHEN player_performance.team = 'home' AND matches.home_score > matches.away_score THEN 1
+		    WHEN player_performance.team = 'away' AND matches.away_score > matches.home_score THEN 1
+		    ELSE NULL
+		    END) AS DECIMAL) * 100.0 / NULLIF(COUNT(player_performance.match_id), 0) 
+    AS FLOAT) AS win_percentage,
+	CAST(COALESCE(SUM(player_performance.goals), 0) AS UNSIGNED) AS goals,
+	CAST(COALESCE(SUM(player_performance.assists), 0) AS UNSIGNED) AS assists
+FROM players
+LEFT JOIN player_performance 
+	ON players.id = player_performance.player_id
+LEFT JOIN matches
+	ON matches.id = player_performance.match_id
+GROUP BY players.id;`;
     try {
         return await db.query(query);
     } catch (error) {
         console.error('Error getting stats: ', error);
-        throw error;
-    }
-}
-
-export async function getTopGoals() {
-    const query =
-    `SELECT
-        players.id AS id,
-	    players.name AS name,
-	    SUM(player_performance.goals) AS goals
-     FROM players
-     LEFT JOIN player_performance 
-	    ON players.id = player_performance.player_id
-     GROUP BY players.id
-     ORDER BY goals DESC;`;
-    try {
-        return await db.query(query);
-    } catch (error) {
-        console.error('Error getting top goals stats: ', error);
-        throw error;
-    }
-}
-
-export async function getTopAssists() {
-    const query =
-        `SELECT
-        players.id AS id,
-	    players.name AS name,
-	    SUM(player_performance.assists) AS assists
-     FROM players
-     LEFT JOIN player_performance 
-	    ON players.id = player_performance.player_id
-     GROUP BY players.id
-     ORDER BY assists DESC;`;
-    try {
-        return await db.query(query);
-    } catch (error) {
-        console.error('Error getting top assists stats: ', error);
-        throw error;
-    }
-}
-
-export async function getTopWinPercentage() {
-    const query =
-    `SELECT
-        players.id AS id,
-	    players.name AS name,
-	    COUNT(player_performance.match_id) as games,
-        COUNT(CASE
-         WHEN player_performance.team = 'home' AND matches.home_score > matches.away_score THEN 1
-         WHEN player_performance.team = 'away' AND matches.away_score > matches.home_score THEN 1
-         ELSE NULL
-         END)/COUNT(player_performance.match_id)*100 AS wins
-     FROM player_performance
-     LEFT JOIN players 
-	    ON players.id = player_performance.player_id
-     LEFT JOIN matches 
-	    ON player_performance.match_id = matches.id
-     GROUP BY players.id
-     ORDER BY wins DESC;`;
-    try {
-        return await db.query(query);
-    } catch (error) {
-        console.error('Error getting top win %: ', error);
-        throw error;
-    }
-}
-
-export async function getTopValue() {
-    const query =
-    `SELECT id, name, value
-     FROM players
-     ORDER BY value DESC;`;
-    try {
-        return await db.query(query);
-    } catch (error) {
-        console.error('Error getting value: ', error);
         throw error;
     }
 }
