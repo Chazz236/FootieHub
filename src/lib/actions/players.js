8c21b925e1baf26ed56859df9fd6bf1ae38a376d
Chazz236 <197831996+Chazz236@users.noreply.github.com>
2026-03-11 17:54:02 -0400

    feat: implement player update server action

--- src/lib/actions/players.js
+++ src/lib/actions/players.js
@@ -1,6 +1,6 @@
 'use server'
 
-import { addPlayer } from '@/db/players';
+import { addPlayer, updatePlayer } from '@/db/players';
 import { revalidatePath } from 'next/cache';
 
 //create a new player to add to database, revalidate players page
@@ -23,5 +23,29 @@ export async function createPlayer(name, joinedAt) {
   } catch (error) {
     console.error('error adding player:', error);
     throw new Error('error adding player');
+  }
+}
+
+//update player, revalidate individual player's page
+export async function updatePlayerId(id, name, joinedAt) {
+
+  //make sure name is real
+  if (!name || typeof name !== 'string' || name.trim() === '') {
+    throw new Error('name must be real');
+  }
+
+  //should have joined date
+  if (!joinedAt) {
+    throw new Error('must have joined date')
+  }
+
+  try {
+    await updatePlayer(id, name, joinedAt);
+    revalidatePath('/players');
+    revalidatePath(`/players/${id}`);
+    return { message: 'player updated' };
+  } catch (error) {
+    console.error('error updating player:', error);
+    throw new Error('error updating player');
   }
 }
