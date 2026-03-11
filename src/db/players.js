abe18c0a6af814cdc9634f833f85453e2f9b4fc2
Chazz236 <197831996+Chazz236@users.noreply.github.com>
2026-03-10 23:20:07 -0400

    feat: add function to update player name and join date

--- src/db/players.js
+++ src/db/players.js
@@ -2,10 +2,10 @@ import db from '@/db/mysql';
 
 //get all data from players table
 export async function getAllPlayers() {
-  
+
   const query =
     `SELECT * FROM players;`;
-  
+
   try {
     return await db.query(query);
   } catch (error) {
@@ -16,10 +16,10 @@ export async function getAllPlayers() {
 
 //get all data for player by id
 export async function getPlayer(id) {
-  
+
   const query =
     `SELECT * FROM players WHERE id = ?;`;
-  
+
   try {
     return await db.query(query, [id]);
   } catch (error) {
@@ -30,15 +30,29 @@ export async function getPlayer(id) {
 
 //add new player into the players table with a starting transfer value of $10,000,000
 export async function addPlayer(name, joinedAt) {
-  
+
   const query =
     `INSERT INTO players (name, value, joinedAt)
 	 VALUES (?, ?, ?)`;
-  
+
   try {
     await db.query(query, [name, 10000000, joinedAt]);
   } catch (error) {
     console.error('Error adding player: ', error);
+    throw error;
+  }
+}
+
+//update player name and join date
+export async function updatePlayer(id, name, joinedAt) {
+
+  const query =
+    `UPDATE players SET name = ?, joinedAt = ? WHERE id = ?`;
+
+  try {
+    await db.query(query, [name, joinedAt, id]);
+  } catch (error) {
+    console.error('Error updating player: ', error);
     throw error;
   }
 }
